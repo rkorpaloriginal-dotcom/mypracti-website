76d2bdaf96951e1b8815bc64ecef282037a32666
R Korpall <rkorpaloriginal@gmail.com>
2026-05-15 12:55:24 +0100

    feat: assemble full MyPracti homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Navbar from '@/components/Navbar'
+import HeroSection from '@/components/HeroSection'
+import WaitlistSection from '@/components/WaitlistSection'
+import ServicesSection from '@/components/ServicesSection'
+import HowItWorksSection from '@/components/HowItWorksSection'
+import ContactSection from '@/components/ContactSection'
+import Footer from '@/components/Footer'
+
 export default function Home() {
-  return <main />
+  return (
+    <main>
+      <Navbar />
+      <HeroSection />
+      <WaitlistSection />
+      <ServicesSection />
+      <HowItWorksSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  )
 }
