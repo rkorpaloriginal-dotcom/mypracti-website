9e83629842be32f7f3916a755346f715a2457feb
R Korpall <rkorpaloriginal@gmail.com>
2026-05-15 12:14:18 +0100

    chore: fix vitest config - exclude .next, add globals types, add user-event

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
     globals: true,
+    exclude: ['node_modules', '.next', 'dist'],
   },
   resolve: {
     alias: {
